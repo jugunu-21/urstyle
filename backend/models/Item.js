e3c443c8ccb16aa3c76f2c340e4b385375f547ec
jugunu <aaruhijugunu@gmail.com>
2024-02-24 12:08:01 +0530

    working on express and routes

---
+++ backend/models/Item.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const itemSchema = new Schema({
+    name: {
+        type: String,
+        required:true 
+    },
+    image_url: {
+        type: String,
+        required:true
+    },
+    price: {
+        type: String,
+        required:true
+    },
+    image_url: {
+        type: String,
+        required:true
+    }
+
+});
+module.exports = mongoose.model('item', itemSchema);
