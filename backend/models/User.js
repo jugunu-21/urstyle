573c635a867b4881bf0030d0c757b74f7f511bd7
jugunu <aaruhijugunu@gmail.com>
2024-02-28 22:05:19 +0530

    models

--- backend/models/User.js
+++ backend/models/User.js
@@ -12,9 +12,8 @@ const userSchema = new Schema({
     },
     email: {
         type: String,
-        required: true,
-        unique:true
-
+        required: true
+    
     },
     date: {
         type:Date,
