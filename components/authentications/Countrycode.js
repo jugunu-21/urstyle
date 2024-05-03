11695df5bc9332f208a4e834eee8e3e170063560
jugunu <aaruhijugunu@gmail.com>
2024-05-04 00:03:59 +0530

    added some more country with its code

--- components/authentications/Countrycode.js
+++ components/authentications/Countrycode.js
@@ -12,36 +12,51 @@ import Countrydata from "./ContextCountryCode";
 const Countrycode = () => {
   const { selectedCountryCode, setSelectedCountryCode } =
     useContext(Countrydata);
-    const handleValueChange = (newValue) => {
-      console.log("New value:", newValue); // Debugging line
-      setSelectedCountryCode(newValue)
-        // console.log(selectedCountryCode);
-    };
+  const handleValueChange = (newValue) => {
+    console.log("New value:", newValue); // Debugging line
+    setSelectedCountryCode(newValue);
+    // console.log(selectedCountryCode);
+  };
   return (
-    <Select  onValueChange={handleValueChange}>
+    <Select onValueChange={handleValueChange}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Country" />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="91"
+        <SelectItem
+          value="91"
           // onClick={() => setSelectedCountryCode("91")}
         >
           India
         </SelectItem>
         <SelectItem
-          value="90"
+          value="43"
           // onClick={() => {
           //   console.log("Africa clicked"); // Debugging line
           //   setSelectedCountryCode("90");
           // }}
         >
-          Africa
+          Austria
         </SelectItem>
-        <SelectItem value="19"
-          
-          // onClick={() => setSelectedCountryCode("19")}
+        <SelectItem
+          value="977"
         >
           Nepal
+        </SelectItem>
+        <SelectItem
+          value="55"
+        >
+          Brazil
+        </SelectItem>
+        <SelectItem
+          value="1"
+        >
+         Canada
+        </SelectItem>
+        <SelectItem
+          value="86"
+        >
+          China
         </SelectItem>
       </SelectContent>
     </Select>
