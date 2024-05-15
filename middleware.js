61a3309a733408e56fd2b0527d98aa4f2627c6b3
jugunu <aaruhijugunu@gmail.com>
2024-05-15 17:19:53 +0530

    some minor change

--- middleware.js
+++ middleware.js
@@ -17,8 +17,16 @@ export default function middleware(request) {
     const intendedRoute = request.nextUrl.pathname;
     console.log(`Intended route: ${intendedRoute}`);
 
-    // Redirect to the sign-out page and pass the intended route
-    return NextResponse.redirect(`/signout${intendedRoute}`);
+    // Redirect to the sign-out page and pass the  url.pathname = `/signout${intendedRoute}`;
+
+  // Return a rewrite response with the absolute URL
+  const url = request.nextUrl.clone();
+
+  // Modify the pathname to include the intended route
+  url.pathname = `/signout${intendedRoute}`;
+
+  // Return a rewrite response with the absolute URL
+  return NextResponse.rewrite(url);
    // Corrected redirect URL
   } else {
     // User is authenticated, proceed with the request
