2c8f4a65b1df59f4d972d73f0d9e7949cffae90e
jugunu-21 <aaruhijugunu@gmail.com>
2024-07-09 21:44:53 +0530

    to add product with api

--- app/(authentications)/productadd/page.tsx
+++ app/(authentications)/productadd/page.tsx
@@ -1,6 +1,6 @@
 "use client "
 import React from 'react';
-import Product from "@/components/storedata/Product"; 
+import Product from "@/components/admin/storedata/Product"; 
 export default function page() {
   return (
   
