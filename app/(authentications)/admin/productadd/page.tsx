a9d2a7974993de114b298a4cd3e38946c81dd98c
jugunu-21 <aaruhijugunu@gmail.com>
2024-07-10 22:49:14 +0530

    created "/admin/productadd" route to add product

---
+++ app/(authentications)/admin/productadd/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+import {Dashboard} from "@/components/admin/productadd/productadd"
+export default function Productadd(){
+    return(
+        <div>
+            <Dashboard/>
+        </div>
+    )
+}
+
+
+
--- app/(authentications)/productadd/page.tsx
+++ app/(authentications)/productadd/page.tsx
@@ -1,6 +1,6 @@
 "use client "
 import React from 'react';
-import Product from "@/components/admin/storedata/Product"; 
+import Product from "@/components/admin/productadd/Product"; 
 export default function page() {
   return (
   
