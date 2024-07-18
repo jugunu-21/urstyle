4e9f4f8bd1e866b8f1cdb8fc64b442838deca7c9
jugunu-21 <aaruhijugunu@gmail.com>
2024-07-18 14:09:39 +0530

    commented the transtack querry

--- components/admin/product/productFunctions/postApiCall.ts
+++ components/admin/product/productFunctions/postApiCall.ts
@@ -44,3 +44,56 @@ import {ProductDataInterface} from "../productutils/productServices/productDataI
     return data;
   }
 export default PostApiCall;
+
+
+
+
+
+
+
+// // 
+// import axios from 'axios';
+// import {ProductDataInterface} from "../productutils/productServices/productDataInterface"
+//   // interface SubmitFunctionArgs {
+//   //   requestBody: ProductRequestBody|null;
+//   //   jwtToken: string; // Assuming jwtToken is a string
+//   // }
+//   async function PostApiCall(args: { requestBody?: ProductDataInterface | null; jwtToken: string ,apiroute:string}) {
+//     const { requestBody, jwtToken,apiroute} = args;
+//     console.log("submitfunction");
+//     console.log("requestBody", requestBody);
+//     interface CustomRequestInit extends RequestInit {
+//       body?: string;
+//     }
+//     // Define the base options for the fetch request
+//     const requestOptions:CustomRequestInit = {
+    
+//       credentials: "include",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${jwtToken}`,
+//       },
+//     };
+  
+//     // Conditionally add the body property if requestBody is not null
+//     if (requestBody !== null) {
+//       requestOptions.body = JSON.stringify(requestBody);
+//     }
+  
+//     const response = await axios.post(
+//       `${process.env.NEXT_PUBLIC_BASE_URL}${apiroute}`,
+//       requestOptions
+//     );
+  
+//     // if (!response.ok) {
+//     //   console.log("error");
+//     //   // Optionally, handle the error here
+//     // } else {
+//     //   console.log("Uploaded product");
+//     //   // Optionally, handle the successful upload here
+//     // }
+//     const data =  response
+//     console.log(data)
+//     return data;
+//   }
+// export default PostApiCall;
