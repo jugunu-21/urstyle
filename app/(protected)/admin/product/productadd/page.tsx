80789cc6ca9957404230e786238af3676a334a8e
jugunu-21 <aaruhijugunu@gmail.com>
2024-08-16 20:14:44 +0530

    "add productadd page"

---
+++ app/(protected)/admin/product/productadd/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React, { useState,useEffect } from 'react';
+
+import Dashboard from "@/components/admin/product/product-add/product-add"
+
+// import ProductImageCard from "@/components/admin/forms/productImage"
+export default function Productadd() {
+   
+    
+    return (
+        <div >
+            <div className="flex min-h-screen w-full flex-col bg-muted/40">
+                <Dashboard />
+            </div>
+        </div>
+
+    )
+}
+
+
+
