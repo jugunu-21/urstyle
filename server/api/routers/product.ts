ada35a4640566a0f02c33ec9499f604853eeeb0a
jugunu-21 <aaruhijugunu@gmail.com>
2024-10-13 19:55:29 +0530

    removed the vercel error

--- server/api/routers/product.ts
+++ server/api/routers/product.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
-import { createTRPCRouter, publicProcedure,protectedProcedure , publicAndProtectedProcedure} from "@/server/api/trpc";
-import {ApiUpdateProduct, ApiUploadImage,ApiFetchProducts,ApiUploadProduct,ApiFetchProductById} from "@/components/admin/product/product-utils/function";
+import { createTRPCRouter, publicProcedure, protectedProcedure, publicAndProtectedProcedure } from "@/server/api/trpc";
+import { ApiUpdateProduct, ApiUploadImage, ApiFetchProducts, ApiUploadProduct, ApiFetchProductById } from "@/components/admin/product/product-utils/function";
 const zproductDataInterface = z.object({
   pid: z.number(),
   name: z.string(),
@@ -10,17 +10,17 @@ const zproductDataInterface = z.object({
   price: z.string(),
   image: z.string() // Assuming image can be null or a string URL
 })
-const simplifiedProducts= z.object({
+const simplifiedProducts = z.object({
   image: z.string(),
-  id:  z.string(),
+  id: z.string(),
   pid: z.number(),
   name: z.string(),
-  code:  z.string(),
+  code: z.string(),
   price: z.string(),
-  link:  z.string(),
+  link: z.string(),
   review: z.array(z.record(z.unknown())),
-  description:  z.string(),
- })
+  description: z.string(),
+})
 
 const apiProductsAddZodSchema = z.object({
   requestBody: zproductDataInterface,
@@ -43,53 +43,70 @@ const paginationSchema = z.object({
 export const productRouter = createTRPCRouter({
   productAdd: protectedProcedure.input(apiProductsAddZodSchema)
     .output(z.object({ message: z.string(), status: z.number() }))
-      .mutation(async ({ ctx, input }) => {
-        const token = ctx.token
-        const modifiedInput={...input,
-          jwtToken:token
-        }
+    .mutation(async ({ ctx, input }) => {
+      const token = ctx.token
+      const modifiedInput = {
+        ...input,
+        jwtToken: token
+      }
       const response = await ApiUploadProduct(modifiedInput)
       console.log("adddproduct")
       return response;
     }),
- 
-    productfetchById:publicAndProtectedProcedure
+
+  productfetchById: publicAndProtectedProcedure
     .input(z.object({
       productId: z.string(),
     }))
-      .output(z.object({ data:z.object({simplifiedProducts:simplifiedProducts}) , message: z.string(), status: z.number() }))
+    .output(z.object({ data: z.object({ simplifiedProducts: simplifiedProducts }), message: z.string(), status: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const token = ctx.token
+      const { productId } = input
+      const modifiedInput = {
+        jwtToken: token,
+        productId
+
+      }
+      console.log(" modifiedInput", modifiedInput)
+      const response = await ApiFetchProductById(modifiedInput)
+      return response;
+    }),
+    productfetch: protectedProcedure
+    .input(paginationSchema)
+      .output(z.object({ data:z.object({simplifiedProducts:z.array(simplifiedProducts),totalDocs:z.number()}) , message: z.string(), status: z.number() }))
       .query(async ({ ctx, input}) => {
         const token = ctx.token
-        const {productId}=input
+        const {page,limit}=input
         const modifiedInput={
           jwtToken:token,
-          productId
-          
+          page:page,
+          limit:limit
         }
         console.log(" modifiedInput", modifiedInput)
-        const response = await ApiFetchProductById(modifiedInput)
+        const response = await ApiFetchProducts(modifiedInput)
         return response;
       }),
-  productUpdate: protectedProcedure.input( apiProductUpdateZodSchema)
-  .output(z.object({data:z.string() , message:z.string() , status:z.number()}))
-    .mutation(async ({ input,ctx }) => {
+  productUpdate: protectedProcedure.input(apiProductUpdateZodSchema)
+    .output(z.object({ data: z.string(), message: z.string(), status: z.number() }))
+    .mutation(async ({ input, ctx }) => {
       const token = ctx.token
-      const modifiedInput={...input,
-        jwtToken:token
+      const modifiedInput = {
+        ...input,
+        jwtToken: token
       }
       const response = await ApiUpdateProduct(modifiedInput)
       console.log("adddproduct")
       return response;
     })
-    // imageUpload: protectedProcedure.input()
-    // .output(z.object({data:z.string() , message:z.string() , status:z.number()}))
-    //   .mutation(async ({ input,ctx }) => {
-    //     const token = ctx.token
-    //     const modifiedInput={...input,
-    //       jwtToken:token
-    //     }
-    //     const response = await  ApiUploadImage(modifiedInput)
-    //     console.log("adddproduct")
-    //     return response;
-    //   })
+  // imageUpload: protectedProcedure.input()
+  // .output(z.object({data:z.string() , message:z.string() , status:z.number()}))
+  //   .mutation(async ({ input,ctx }) => {
+  //     const token = ctx.token
+  //     const modifiedInput={...input,
+  //       jwtToken:token
+  //     }
+  //     const response = await  ApiUploadImage(modifiedInput)
+  //     console.log("adddproduct")
+  //     return response;
+  //   })
 })
