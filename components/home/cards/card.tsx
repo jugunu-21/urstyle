0d5c12e148a898698e01cb3956425a16f14bdca0
jugunu-21 <aaruhijugunu@gmail.com>
2024-10-22 16:39:51 +0530

    add background colour if there is no image in card

--- components/home/cards/card.tsx
+++ components/home/cards/card.tsx
@@ -36,7 +36,7 @@ export const Card = ({ productColl, refetch }: { productColl: ProductCollection,
         <Link className=" bg-stone-200 rounded-lg hover:bg-stone-300 text-lg font-semibold text-neutral-950" href={`/details/${ productColl.collectionId}`}>
         <div className="my-4 p-2  ">
             <div>{productColl.name}</div>
-            <div className=" relative h-72 md:h-80    grid grid-cols-3">
+            <div className=" relative h-72 md:h-80 grid grid-cols-3">
                 {/* {productColl.products.map((product, index) => (
                     <div key={product.id} className={`  ${index === 0 ? 'col-span-2' : ''}${index === 1 ? 'col-span-1 grid-rows-3  ' : ''}  my-2`}>
                         <div style={backgroundStyle(product.image)} className={` ${index === 0 ? 'h-full py-2 border-violet-800 row-span-1' : 'row-span-1 p-2 border-sky-500'}`}>{index}
@@ -47,18 +47,40 @@ export const Card = ({ productColl, refetch }: { productColl: ProductCollection,
                 <div className="col-span-2" style={backgroundStyle(productColl.products[0].image)}>
                 </div>
                 <div className=" col-span-1 grid grid-rows-3 ">
-                    <div className=" mx-2 mb-2">
-                        <div className="row-span-1   " style={backgroundStyle(productColl.products[1].image)}>
+                {productColl.products[1]?<div className=" mx-2 mt-2">
+                        <div className="row-span-1  " style={backgroundStyle(productColl.products[1]?.image)}>
                         </div>
+                    </div>:<>
+                    <div className="bg-red-100 h-full  mx-2 mt-2 ">
+                        <div  className="row-span-1  "></div>
+                     
                     </div>
-                    <div className=" mx-2 my-1">
+                        
+                    </>}
+                    {productColl.products[2]?<div className=" mx-2 mt-2">
                         <div className="row-span-1  " style={backgroundStyle(productColl.products[2]?.image)}>
                         </div>
+                    </div>:<>
+                    <div className="bg-red-100  mx-2 mt-2 ">
+                        <div  className="row-span-1  "></div>
+                     
                     </div>
-                    <div className=" mx-2 mt-2">
+                        
+                    </>}
+                    {productColl.products[3]?<div className=" mx-2 mt-2">
                         <div className="row-span-1  " style={backgroundStyle(productColl.products[3]?.image)}>
                         </div>
+                    </div>:<>
+                    <div className="bg-red-100   mx-2 mt-2 ">
+                        <div  className="row-span-1  h-full w-full  "></div>
+                     
                     </div>
+                        
+                    </>}
+                    {/* <div className=" mx-2 mt-2">
+                        <div className="row-span-1  " style={backgroundStyle(productColl.products[3]?.image)}>
+                        </div>
+                    </div> */}
                 </div>{productColl.products.length > 4 && <div className="absolute bottom-10 right-4 "><FiPlus className=" h-8 w-8    p-1" /></div>}
                 {productColl.hasOwnProperty('likestatus') &&
                     <button className="absolute bottom-0  right-1 "  onClick={(e) => {
